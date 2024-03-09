a7e76035726d9bed1a0586afa9728b089cf14319
byMagg <byMag@hotmail.es>
2024-03-09 22:16:17 +0100

    Cambiado puerto

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import { connect } from "./db";
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+import { connect } from './db'
 
-import { typeDefs } from "./models/typeDef";
-import { resolvers } from "./resolvers";
+import { typeDefs } from './models/typeDef'
+import { resolvers } from './resolvers'
 
-const port = Number(process.env.PORT) || 3000;
+const port = Number(process.env.PORT) || 4321
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers })
 
 const startServer = async () => {
   try {
     const { url } = await startStandaloneServer(server, {
       listen: { port },
-    });
-    console.log(`Server ready at ${url}`);
+    })
+    console.log(`Server ready at ${url}`)
   } catch (error: any) {
-    console.error("Error starting server: ", error.message);
+    console.error('Error starting server: ', error.message)
   }
-};
+}
 
-startServer();
-connect();
+startServer()
+connect()
