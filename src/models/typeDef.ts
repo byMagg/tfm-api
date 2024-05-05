03923e616442503cbde1c028915b5c1fb79e5b35
byMagg <byMag@hotmail.es>
2024-05-05 17:51:20 +0200

    Cleanup Accident

--- src/models/typeDef.ts
+++ src/models/typeDef.ts
@@ -2,70 +2,13 @@ import { gql } from 'graphql-tag'
 
 export const typeDefs = gql`
   type Query {
-    accidentCount: Int
-    getAccidents(limit: Int, offset: Int): [Accident]
-    getAccidentById(accidentId: String!): Accident
-    getAccidentsBySeverity(
-      severity: String!
-      limit: Int
-      offset: Int
-    ): [Accident]
-
     matchesCount: Int
     getMatches(limit: Int, offset: Int): [Match]
-  }
-
-  type Accident {
-    _id: String
-    ID: String
-    Source: String
-    Severity: String
-    Start_Time: String
-    End_Time: String
-    Start_Lat: String
-    Start_Lng: String
-    End_Lat: String
-    End_Lng: String
-    Distance: String
-    Description: String
-    Street: String
-    City: String
-    County: String
-    State: String
-    Zipcode: String
-    Country: String
-    Timezone: String
-    Airport_Code: String
-    Weather_Timestamp: String
-    Temperature: String
-    Wind_Chill: String
-    Humidity: String
-    Pressure: String
-    Visibility: String
-    Wind_Direction: String
-    Wind_Speed: String
-    Precipitation: String
-    Weather_Condition: String
-    Amenity: String
-    Bump: String
-    Crossing: String
-    Give_Way: String
-    Junction: String
-    No_Exit: String
-    Railway: String
-    Roundabout: String
-    Station: String
-    Stop: String
-    Traffic_Calming: String
-    Traffic_Signal: String
-    Turning_Loop: String
-    Sunrise_Sunset: String
-    Civil_Twilight: String
-    Nautical_Twilight: String
-    Astronomical_Twilight: String
+    getMatchById(matchId: String!): Match
   }
 
   type Match {
+    _id: ID
     tourney_id: String
     tourney_name: String
     surface: String
@@ -147,6 +90,5 @@ export const typeDefs = gql`
     loser1_rank_points: String
     loser2_rank: String
     loser2_rank_points: String
-    _id: ID
   }
 `
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,32 +1,7 @@
-import { Accident } from './models/Accident'
 import { Match } from './models/Match'
 
 export const resolvers = {
   Query: {
-    accidentCount: async () => {
-      return await Accident.estimatedDocumentCount()
-    },
-    getAccidents: async (
-      _: any,
-      { limit = 10, offset = 0 }: { limit: number; offset: number }
-    ) => {
-      return await Accident.find().limit(limit).skip(offset)
-    },
-    getAccidentById: async (_: any, { accidentId }: { accidentId: string }) => {
-      return await Accident.findOne({ ID: accidentId })
-    },
-    getAccidentsBySeverity: async (
-      _: any,
-      {
-        severity,
-        limit = 10,
-        offset = 0,
-      }: { severity: string; limit: number; offset: number }
-    ) => {
-      return await Accident.find({ Severity: severity })
-        .limit(limit)
-        .skip(offset)
-    },
     matchesCount: async () => {
       return await Match.estimatedDocumentCount()
     },
@@ -35,6 +10,9 @@ export const resolvers = {
       { limit = 10, offset = 0 }: { limit: number; offset: number }
     ) => {
       return await Match.find().limit(limit).skip(offset)
+    },
+    getMatchById: async (_: any, { matchId }: { matchId: string }) => {
+      return await Match.findOne({ _id: matchId })
     },
   },
 }
