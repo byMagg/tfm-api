5d2ed0b7c7b8d60887f2829a6155b6a7485764de
byMagg <byMag@hotmail.es>
2025-03-26 12:15:41 +0100

    feat: Añadir rutas para crear ligas, iniciar temporadas y generar partidos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -34,12 +34,124 @@ router.get('/leagues/:id', async (req, res) => {
 })
 
 router.post('/leagues', async (req, res) => {
-  const league = new League(req.body)
-  await league.save()
+  const { name } = req.body
+
+  if (!name) {
+    return sendError({
+      res,
+      statusCode: 400,
+      message: 'Debes enviar un nombre',
+    })
+  }
+
+  const league = await League.create({ name })
+
+  const startedAt = new Date()
+
+  if (startedAt.getMonth() === 11) {
+    startedAt.setMonth(0)
+    startedAt.setFullYear(startedAt.getFullYear() + 1)
+  }
+
+  for (let i = startedAt.getMonth(); i < 12; i++) {
+    startedAt.setMonth(i)
+    startedAt.setDate(1)
+    startedAt.setHours(0, 0, 0, 0)
+
+    await Season.create({
+      startedAt: startedAt,
+      league_id: league._id,
+    })
+  }
+
   sendResponse({
     res,
     statusCode: 201,
     data: league,
+  })
+})
+
+router.post('/leagues/:id/start', async (req, res) => {
+  const { id } = req.params
+  const league = await League.findOneAndUpdate(
+    { _id: id },
+    { startedAt: new Date() },
+    { new: true }
+  )
+
+  if (!league) {
+    return sendError({
+      res,
+      statusCode: 404,
+      message: 'League not found',
+    })
+  }
+
+  sendResponse({
+    res,
+    data: league,
+  })
+})
+
+router.post('/leagues/:id/matches', async (req, res) => {
+  const { id } = req.params
+  const league = await League.findById(id)
+
+  if (!league) {
+    return sendError({
+      res,
+      statusCode: 404,
+      message: 'League not found',
+    })
+  }
+
+  const now = new Date()
+  const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1)
+  const endOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0)
+
+  const currentSeason = await Season.findOne({
+    league_id: league._id,
+    startedAt: {
+      $gte: startOfMonth,
+      $lt: endOfMonth,
+    },
+  })
+
+  if (!currentSeason) {
+    return sendError({
+      res,
+      statusCode: 404,
+      message: 'Season not found',
+    })
+  }
+
+  const players = league.players
+
+  if (players.length < 2) {
+    return sendError({
+      res,
+      statusCode: 400,
+      message: 'Not enough players',
+    })
+  }
+
+  const matches = []
+
+  for (let i = 0; i < players.length; i++) {
+    for (let j = i + 1; j < players.length; j++) {
+      matches.push({
+        season_id: currentSeason._id,
+        player1: players[i],
+        player2: players[j],
+      })
+    }
+  }
+
+  const response = await LeagueMatch.insertMany(matches)
+
+  sendResponse({
+    res,
+    data: response,
   })
 })
 
@@ -76,9 +188,9 @@ router.post('/leagues/:id/players', async (req, res) => {
 
 router.get('/leagues/players/:playerId', async (req, res) => {
   const { playerId } = req.params
-  const league = await League.findOne({ players: playerId })
+  const leagues = await League.find({ players: playerId })
 
-  if (!league) {
+  if (!leagues) {
     return sendError({
       res,
       statusCode: 404,
@@ -90,25 +202,33 @@ router.get('/leagues/players/:playerId', async (req, res) => {
   const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1)
   const endOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0)
 
-  const currentSeason = await Season.findOne({
-    league_id: league._id,
-    startedAt: {
-      $gte: startOfMonth,
-      $lt: endOfMonth,
-    },
-  })
+  let mergedLeagues = []
 
-  const matches = await LeagueMatch.find({
-    season_id: currentSeason?._id,
-    player1: playerId,
-  })
+  for (const league of leagues) {
+    const currentSeason = await Season.findOne({
+      league_id: league._id,
+      startedAt: {
+        $gte: startOfMonth,
+        $lt: endOfMonth,
+      },
+    })
+
+    const matches = await LeagueMatch.find({
+      season_id: currentSeason?._id,
+      player1: playerId,
+    })
+
+    mergedLeagues.push({
+      ...league.toObject(),
+      currentSeason,
+      matches,
+    })
+  }
 
   sendResponse({
     res,
     data: {
-      league,
-      currentSeason,
-      matches,
+      leagues: mergedLeagues,
     },
   })
 })
