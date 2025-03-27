477c75204f3162af3e017b5df04b0172a70b1528
byMagg <byMag@hotmail.es>
2025-03-27 18:54:48 +0100

    feat: Añadir soporte para WebSocket utilizando Socket.IO

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import cors from 'cors'
 import * as dotenv from 'dotenv'
 import express from 'express'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
 import { connect } from './db'
 import router from './routes'
 
 dotenv.config()
 
 const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer, {})
 const port = Number(process.env.PORT) || 3000
 
 app.use(express.json())
@@ -17,6 +21,13 @@ connect()
 
 app.use('/api', router)
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
+
+httpServer.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
 })
